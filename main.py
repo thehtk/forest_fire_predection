08eec52c75c09e9151ed5de7f5057c3bf6e6fd1a
HITESH KUMAR <73693548+thehtk@users.noreply.github.com>
2022-09-18 10:52:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 14:06:47 2022
+
+@author: Hp
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv('C:\\Users\\Hp\\OneDrive\\Projects\\deploy\\Forest_fire.csv')
+data = np.array(data)
+
+X = data[1:, 1:-1]
+y = data[1:, -1]
+y = y.astype('int')
+X = X.astype('int')
+# print(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+log_reg = LogisticRegression()
+
+
+log_reg.fit(X_train, y_train)
+
+pickle.dump(log_reg,open('model.pkl','wb'))
+
+
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[50,30,65]]))
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 14:07:56 2022
+
+@author: Hp
+"""
+
+import streamlit as st
+import pickle
+import numpy as np
+import os
+print(os.getcwd())
+import time
+###time.sleep(30)
+model=pickle.load(open('model.pkl','rb'))
+
+
+def predict_forest(oxygen,humidity,temperature):
+    input=np.array([[oxygen,humidity,temperature]]).astype(np.float64)
+    prediction=model.predict_proba(input)
+    pred='{0:.{1}f}'.format(prediction[0][0], 2)
+    return float(pred)
+
+def main():
+    st.title("Streamlit Tutorial")
+    html_temp = """
+    <div style="background-color:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Forest Fire Prediction ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    oxygen = st.text_input("Oxygen","Type Here")
+    humidity = st.text_input("Humidity","Type Here")
+    temperature = st.text_input("Temperature","Type Here")
+    safe_html="""  
+      <div style="background-color:#F4D03F;padding:10px >
+       <h2 style="color:white;text-align:center;"> Your forest is safe</h2>
+       </div>
+    """
+    danger_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> Your forest is in danger</h2>
+       </div>
+    """
+
+    if st.button("Predict"):
+        output=predict_forest(oxygen,humidity,temperature)
+        st.success('The probability of fire taking place is {}'.format(output))
+
+        if output > 0.5:
+            st.markdown(danger_html,unsafe_allow_html=True)
+        else:
+            st.markdown(safe_html,unsafe_allow_html=True)
+
+if __name__=='__main__':
+    main()
